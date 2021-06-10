5b7144a4d290ba046a86ba03cb89ad0043fcb6b2
anduser <initial commit>
2021-06-10 08:42:30 +0300

    work3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,26 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+
+        List<String> words = Arrays.asList(
+                "cat", "dog", "rat", "hamster", "pig",
+                "elephant", "giraffe", "cat", "dog", "bunny",
+                "pig", "rat", "elephant", "bird", "parrot",
+                "dog", "bunny", "giraffe", "hamster", "cat"
+        );
+
+        Set<String> unique = new HashSet<String>(words);
+
+        System.out.println("Start array: ");
+        System.out.println(words.toString());
+        System.out.println("Unique words");
+        System.out.println(unique.toString());
+        System.out.println("Number of occurrence words: ");
+        for (String key : unique) {
+            System.out.print(key + "-" + Collections.frequency(words, key) + ", ");
+        }
     }
 }
